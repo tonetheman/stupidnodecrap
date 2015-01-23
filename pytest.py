641a0e9ac854b2a7a2bf14b46eb1930be0681f93
tonetheman <tonetheman@gmail.com>
2015-01-22 23:17:27 -0600

    more comments

--- pytest.py
+++ pytest.py
@@ -3,6 +3,8 @@ import httplib
 
 httplib.HTTPConnection.debuglevel = 1
 
+# python encodes this as a bunch of url encoded K=V pairs
+# in the body
 data = { "a" : 10, "b" : 20 }
 url = "http://localhost:9000/"
 r = requests.post(url,data)
